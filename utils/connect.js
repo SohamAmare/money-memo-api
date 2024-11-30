b12bb135c6e6871c74fcd58476641bd134836bd0
SohamAmare <sohamamare07@gmail.com>
2024-11-30 20:02:23 +0530

    l1 commit

--- utils/connect.js
+++ utils/connect.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb://127.0.0.1:27017/moneyMemoDB"
+      // "mongodb://127.0.0.1:27017/moneyMemoDB"
+      process.env.MONGO_URI
     );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
