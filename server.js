be2bcc303e4034170e05926a57b7e00fa1c3c2ab
SohamAmare <sohamamare07@gmail.com>
2024-11-30 19:46:31 +0530

    l commit

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Frontend URL
+  })
+);
 app.use(express.json());
 
 app.use("/api", UserRoute);
